b9a2124a24d94e64ecb736ff9816107a154dbb14
niyati0019 <niyati0019@gmail.com>
2026-05-29 20:43:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'ssc-war-v2';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      './',
+      './index.html',
+      './sw.js',
+      './manifest.json'
+    ]).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached || caches.match('./index.html'));
+    })
+  );
+});
